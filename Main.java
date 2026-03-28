8b275388b7ed4784646e005d2ded624a61599411
dolevTB <dolevgeva@gmail.com>
2026-03-28 15:52:15 +0300

    changed main, drank 3 cups of coffee and 1 energy drink

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Scalar s1 = new IntegerScalar(5);
         Scalar s2 = new RationalScalar(1,2);
-        System.out.println("s1: " + s1.toString() + "s2: " + s2.toString());
-        
+        System.out.println("s1: " + s1.toString());
+        System.out.println("s2: " + s2.toString());
     }
 }
